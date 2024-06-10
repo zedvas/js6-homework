d0204101f07438291a747fdc5f823d30bb1d5e27
zedvas <82768795+zedvas@users.noreply.github.com>
2024-06-10 17:50:00 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// Classwork:
+// Push forwards with the weather project
+// Create a class
+// Extend the first class twice
+// Extend one of the above classes a third time
+// Use any theme/real life thing you like :+1:
